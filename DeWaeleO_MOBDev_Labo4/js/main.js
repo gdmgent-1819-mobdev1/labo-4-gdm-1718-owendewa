6b7456e8f92cc09eaca80fe60f9e4edebfdc848e
Owen De Waele <owendewa@student.arteveldehs.be>
2018-11-13 22:26:44 +0100

    Update DeWaeleO_MOBDev_Labo4/js/main.js

--- DeWaeleO_MOBDev_Labo4/js/main.js
+++ DeWaeleO_MOBDev_Labo4/js/main.js
@@ -271,6 +271,32 @@ fetch('https://randomuser.me/api/?results=10')// max 10 resultaten bij de fetch
 				});
 			});
 		}
+		document.addEventListener('DOMContentLoaded', function () {
+			if (!Notification) {
+			  alert('Desktop notifications not available in your browser. Try Chromium.'); 
+			  return;
+			}
+		  
+			if (Notification.permission !== "granted")
+			  Notification.requestPermission();
+		  });
+		  
+		  function notifyMe() {
+			if (Notification.permission !== "granted")
+			  Notification.requestPermission();
+			else {
+			  var notification = new Notification('Notification title', {
+				icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+				body: "Hey there! You've been notified!",
+			  });
+		  
+			  notification.onclick = function () {
+				window.open("http://stackoverflow.com/a/13328397/1269037");      
+			  };
+		  
+			}
+		  
+		  }
 	}).catch(function(error){
 		console.log('de data word niet weergegeven ' + error.message);
 	})
